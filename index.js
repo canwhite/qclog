06b1be64c6360470ee15a0d0e3530ef01c18dd14
qiaochao <752716886@qq.com>
2021-07-13 17:49:31 +0800

    des

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 class QCLog{
-    //构造函数
     constructor(env){
         this.env = env;
     }
     static getInstance(env){
-        //判断时候已经new过一个实例
         if(!QCLog.instance){
             this.env = env;
-            //若是这个唯一的实例不存在，就创建它
             QCLog.instance = new QCLog(env);
         }
         return QCLog.instance;
     }
-    //对外方法
     log(data){
         console.log("this.env",this.env);
         if(this.env == "development"){
@@ -21,7 +17,6 @@ class QCLog{
             console.log("...");
         }
     }
-
 }
 export default QCLog;
 
